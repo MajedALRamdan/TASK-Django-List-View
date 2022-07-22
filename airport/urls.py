d228ceabccf09fe5bda87a477a435f9ebcb5d424
MajedALRamdan <majooddark237514@gmail.com>
2022-07-22 20:19:48 +0300

    done

--- airport/urls.py
+++ airport/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-
+from flights.views import FlightsShow, BookingUpcoming
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
+    #api urls
+    path("flights-list/",FlightsShow.as_view()),
+    path("bookings-list/",BookingUpcoming.as_view(),),
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,16 @@
+
+from django.utils import timezone
+
 from .models import Booking, Flight
+from .serializers import ListSerializer, ListSerializerBooking
+from rest_framework.generics import ListAPIView
+
+
+class FlightsShow(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = ListSerializer
+
+class BookingUpcoming(ListAPIView):
+
+    queryset = Booking.objects.all().filter(date__gt= timezone.now())
+    serializer_class = ListSerializerBooking
